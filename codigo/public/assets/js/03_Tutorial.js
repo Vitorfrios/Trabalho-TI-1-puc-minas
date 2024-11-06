9741819889fdf688a67236726be792c2688bd891
Vitorfrios <vfrios@sga.pucminas.br>
2024-11-06 19:38:45 -0300

    del

--- codigo/public/assets/js/03_Tutorial.js
+++
@@ -1,5 +0,0 @@
-
-/*
-COPIAR CODIGO NO TERMINAL PARA INICIALIZAR O JSON SERVER
-npx json-server --watch codigo/db/db.json --port 3000
-*/
--- commit_files.ps1
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Para cada arquivo modificado, faça um commit separado
-for file in $(git status --porcelain | grep '^ M' | cut -d ' ' -f 2); do
-    git add "$file"
-    git commit -m "Commit para o arquivo $file"
-done
